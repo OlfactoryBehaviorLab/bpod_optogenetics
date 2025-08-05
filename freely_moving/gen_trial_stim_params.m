e3b3337f24c748f1d8494d056d268a4431da9b57
Austin Pauley <austin2118ace@gmail.com>
2025-08-05 14:18:30 -0400

    generate position index to go with the position in um

--- freely_moving/gen_trial_stim_params.m
+++ freely_moving/gen_trial_stim_params.m
@@ -1,6 +1,11 @@
 function params = gen_trial_stim_params(num_trials_per_combo, position, power)
-    possible_combos = combinations(position, power);
+    position_indices = 1:length(position);
+    possible_combos = combinations(position_indices, power);
     repeat_combos = repelem(possible_combos, num_trials_per_combo, 1);
+    repeat_combos.position_um = zeros(size(repeat_combos, 1), 1);
+    for i = 1:length(position_indices)
+        repeat_combos.position_um(repeat_combos.position_indices == i) = position(i);
+    end
     rand_row_order = randperm(size(repeat_combos, 1));
     params = repeat_combos(rand_row_order, :);
 end
