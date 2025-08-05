8f1a949f55f819b0fa85bac9367bfc90d74c2d34
Austin Pauley <austin2118ace@gmail.com>
2025-08-05 16:29:02 -0400

    rename values in table

--- freely_moving/gen_trial_stim_params.m
+++ freely_moving/gen_trial_stim_params.m
@@ -1,10 +1,10 @@
 function params = gen_trial_stim_params(num_trials_per_combo, position, power)
-    position_indices = 1:length(position);
-    possible_combos = combinations(position_indices, power);
+    position_index = 1:length(position);
+    possible_combos = combinations(position_index, power);
     repeat_combos = repelem(possible_combos, num_trials_per_combo, 1);
     repeat_combos.position_um = zeros(size(repeat_combos, 1), 1);
-    for i = 1:length(position_indices)
-        repeat_combos.position_um(repeat_combos.position_indices == i) = position(i);
+    for i = 1:length(position_index)
+        repeat_combos.position_um(repeat_combos.position_index == i) = position(i);
     end
     rand_row_order = randperm(size(repeat_combos, 1));
     params = repeat_combos(rand_row_order, :);
