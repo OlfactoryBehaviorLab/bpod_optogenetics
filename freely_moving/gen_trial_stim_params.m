7d094bfe69ce1e6e7ab2aeee6e6aed7f25028610
Austin Pauley <austin2118ace@gmail.com>
2025-10-27 15:08:16 -0400

    Add null trials to trials

--- freely_moving/gen_trial_stim_params.m
+++ freely_moving/gen_trial_stim_params.m
@@ -6,6 +6,14 @@ function params = gen_trial_stim_params(num_trials_per_combo, position, power)
     for i = 1:length(position_index)
         repeat_combos.position_um(repeat_combos.position_index == i) = position(i);
     end
+    
+    null_trial = [];
+    null_trial.position_index = 0;
+    null_trial.power = 0;
+    null_trial.position_um = 0;
+
+    null_trials = struct2table(repelem(null_trial, num_trials_per_combo, 1));
+    repeat_combos = [repeat_combos; null_trials];
     rand_row_order = randperm(size(repeat_combos, 1));
     params = repeat_combos(rand_row_order, :);
 end
